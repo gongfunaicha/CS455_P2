59e4f27fd881c15a679fb0c45fd279277d9434e9
Chen Wang <gongfunaicha@gmail.com>
2017-02-27 19:10:16 -0700

    Changed the Task into class.

--- src/cs455/scaling/task/Task.java
+++ src/cs455/scaling/task/Task.java
@@ -1,5 +1,25 @@
 package cs455.scaling.task;
 
-public interface Task {
-    String getType();
+import java.nio.channels.SelectionKey;
+
+public class Task {
+    private char task = 0;
+    private SelectionKey key = null;
+
+    // 'R': Read, 'H': Hash, 'W': Write
+    public Task(char task, SelectionKey key)
+    {
+        this.task = task;
+        this.key = key;
+    }
+
+    public char getTask()
+    {
+        return task;
+    }
+
+    public SelectionKey getKey()
+    {
+        return key;
+    }
 }
